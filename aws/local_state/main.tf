b4551982bdbeafd18454fb24b3239a372595b4b4
nikhilvshinde0404 <104597446+nikhilvshinde0404@users.noreply.github.com>
2023-06-02 12:30:25 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.4.6"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0607784b46cbe5816"
   instance_type = "t2.micro"
 
   tags = {
